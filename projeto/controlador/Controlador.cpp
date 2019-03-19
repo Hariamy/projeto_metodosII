f97486d2629bc251be4a33d46d8a7707e63ca726
Hariamy <hariamymv@hotmail.com>
2019-03-19 17:57:11 -0300

    Arquivocontrolador

---
+++ projeto/controlador/Controlador.cpp
@@ -0,0 +1,5 @@
+#include "../modelo/Integral.h"
+#include "../modelo/NewtonGates.h"
+#include "../modelo/funcoes/Funcao.h"
+#include "../modelo/funcoes/Generica.h"
+
--- projeto/visao/Main.cpp
+++ projeto/visao/Main.cpp
@@ -1,7 +1,4 @@
-#include "../modelo/Integral.h"
-#include "../modelo/NewtonGates.h"
-#include "../modelo/funcoes/Funcao.h"
-#include "../modelo/funcoes/Generica.h"
+
 
 int main(int argc, char const *argv[])
 {

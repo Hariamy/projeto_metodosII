425c1e8d617a9b3a1924215ec927519304cd4481
castelojb <joaocb14@gmail.com>
2019-04-25 16:23:11 -0300

    [#2] remocao codigo c++

--- projeto/modelo/funcoes/Funcao.cpp
+++
@@ -1,7 +0,0 @@
-#include "Funcao.h"
-
-Funcao::Funcao(){}
-
-float Funcao::calcular(float x){
-	return -1;
-}
--- projeto/modelo/funcoes/Seno.cpp
+++
@@ -1,9 +0,0 @@
-#include "Seno.h"
-
-using namespace std;
-
-Seno::Seno(){};
-
-float Seno::calcular(float x){
-	return sin(x);
-}

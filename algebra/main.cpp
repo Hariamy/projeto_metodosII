7189393c6cd6d5f58553f90740278c6e2b2a5a5b
icaroslb <icrsbarbosa@gmail.com>
2019-05-02 00:33:25 -0300

    Criação de biblioteca para matrizes e vetores e criação do método da potência

---
+++ algebra/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cmath>
+
+#include "algebra.h"
+#include "metodos.h"
+
+int main(){
+	float **valores, erro;
+	int tamMatriz;
+	
+	std::cout << "Insira o tamanho da matriz: ";
+	std::cin >> tamMatriz;
+	
+	valores = (float**)malloc(sizeof(float*) * tamMatriz);
+	
+	for(int i = 0; i < tamMatriz; i++){
+		valores[i] = (float*)malloc(sizeof(float) * tamMatriz);
+		for(int j = 0; j < tamMatriz; j++){
+			std::cout << "mat[" << i <<", " << j << "] = ";
+			std::cin >> valores[i][j];
+		}
+	}
+	
+	std::cout << "Insira o valor do erro: ";
+	std::cin >> erro;
+	
+	alg::matriz mt(tamMatriz, valores);
+	
+	met::resposta respo = met::exponencial(mt, erro);
+	
+	std::cout << "Auto valor: " << respo.autoValor << "\nAuto vetor: ";
+	
+	for(int i = 0; i < tamMatriz; i++){
+		std::cout << respo.autoVetor[i] << ' '; 
+	}
+	std::cout << '\n';
+	
+	return 0;
+}
---
+++ algebra/metodos.h
@@ -0,0 +1,43 @@
+#ifndef METODOS_H
+#define METODOS_H
+
+#include <iostream>
+#include <cmath>
+#include "algebra.h"
+
+namespace met{
+	struct resposta{
+		float autoValor;
+		alg::vetor autoVetor;
+	};
+	
+	inline resposta exponencial(alg::matriz mat, float erro){
+		float *vet = (float*)malloc(sizeof(float) * mat.tam),
+		respoVal = INFINITY, anterior;
+		
+		vet[0] = 1;
+		
+		for(int i = 1; i < mat.tam; i++){
+			vet[i] = 0;
+		}
+		
+		alg::vetor respoVet(mat.tam, vet), aux(mat.tam, NULL);
+		do{
+			anterior = respoVal;
+			
+			aux = mat * respoVet;
+			for(int i = 0; i < mat.tam; i++){std::cout << respoVet[i] << " * ";} std::cout << '\n';
+			respoVal = respoVet * aux;
+			
+			respoVet = aux.valores;
+			respoVet.unitario();
+			for(int i = 0; i < mat.tam; i++){std::cout << respoVet[i] << "  ";} std::cout << '\n';
+		}while(std::abs(respoVal - anterior) > erro);
+		
+		resposta respo{respoVal, respoVet};
+		
+		return respo;
+	}
+}
+
+#endif //METODOS_H

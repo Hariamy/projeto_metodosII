91dca37512f5f8903da6f0f0b06d4d63d4778667
icaroslb <icrsbarbosa@gmail.com>
2019-05-14 15:12:06 -0300

    Correção de bugs no parser e criação de um verificador de expressão

--- algebra/testeIntegral.cpp
+++ algebra/testeIntegral.cpp
@@ -7,8 +7,13 @@
 int main(){
     float intervalo[2] = {0, (PI / 2)};
     
-    std::string tt = "1+3-senx";
-    expre::expre *calc = parser::parser(tt, 0, tt.size());
+    std::string tt = "1-3.2-senx";
+    if(parser::analisador(tt)){
+        expre::expre *calc = parser::parser(tt, 0, tt.size());
 
-    std::cout << inte::newtonCotes(4, ABERTA, 1, intervalo, calc) << '\n';
+        std::cout << inte::newtonCotes(2, ABERTA, 1, intervalo, calc) << '\n';
+        std::cout << calc->calcular(2) << '\n';
+    }else{
+        std::cout << "Não foi\n";
+    }
 }

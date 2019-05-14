88c41eda1419368782150824ecce830c896fda01
icaroslb <icrsbarbosa@gmail.com>
2019-05-14 17:19:15 -0300

    Termino das funções de Gauss Legendre

--- algebra/integral.h
+++ algebra/integral.h
@@ -14,6 +14,29 @@ namespace inte {
     #define FECHADA  0
     #define ABERTA   1
 
+    #define t11 0.0
+    #define t12 (std::sqrt(3.0) / 3.0)
+    #define t22 -(std::sqrt(3.0) / 3.0)
+    #define t13 0.0
+    #define t23 (std::sqrt(3.0 / 5.0))
+    #define t33 -(std::sqrt(3.0 / 5.0))
+    #define t14 (std::sqrt((3.0 - (2.0 * (std::sqrt(6.0 / 5.0)))) / 7.0))
+    #define t24 -(std::sqrt((3.0 - (2.0 * (std::sqrt(6.0 / 5.0)))) / 7.0))
+    #define t34 (std::sqrt((3.0 + (2.0 * (std::sqrt(6.0 / 5.0)))) / 7.0))
+    #define t44 -(std::sqrt((3.0 + (2.0 * (std::sqrt(6.0 / 5.0)))) / 7.0))
+
+    #define w11 2.0
+    #define w12 1.0
+    #define w22 1.0
+    #define w13 (8.0 / 9.0)
+    #define w23 (5.0 / 9.0)
+    #define w33 (5.0 / 9.0)
+    #define w14 ((18.0 + std::sqrt(30.0)) / 36.0)
+    #define w24 ((18.0 + std::sqrt(30.0)) / 36.0)
+    #define w34 ((18.0 - std::sqrt(30.0)) / 36.0)
+    #define w44 ((18.0 - std::sqrt(30.0)) / 36.0)
+    
+
     inline float newtonCotes(const int &grau, const int &filosofia, const int &qtdDivisoes, const float intervalo[2], expre::expre *equacao);
 
     inline float* initNcG1 (const int &filosofia, const int &qtdDivisoes, const float intervalo[2], float &h);
@@ -21,18 +44,31 @@ namespace inte {
     inline float* initNcG3 (const int &filosofia, const int &qtdDivisoes, const float intervalo[2], float &h);
     inline float* initNcG4 (const int &filosofia, const int &qtdDivisoes, const float intervalo[2], float &h);
 
-    inline float newtonCotesG1(const int &filosofia, const float &inicio, const float &fim, const float &h, expre::expre *equacao);
-    inline float newtonCotesG2(const int &filosofia, const float &inicio, const float &fim, const float &h, expre::expre *equacao);
-    inline float newtonCotesG3(const int &filosofia, const float &inicio, const float &fim, const float &h, expre::expre *equacao);
-    inline float newtonCotesG4(const int &filosofia, const float &inicio, const float &fim, const float &h, expre::expre *equacao);
+    inline float newtonCotesG1 (const int &filosofia, const float &inicio, const float &fim, const float &h, expre::expre *equacao);
+    inline float newtonCotesG2 (const int &filosofia, const float &inicio, const float &fim, const float &h, expre::expre *equacao);
+    inline float newtonCotesG3 (const int &filosofia, const float &inicio, const float &fim, const float &h, expre::expre *equacao);
+    inline float newtonCotesG4 (const int &filosofia, const float &inicio, const float &fim, const float &h, expre::expre *equacao);
+
+    inline float gaussLegendre (const int &qtdPontos, const int &qtdDivisoes, const float intervalo[2], expre::expre *equacao);
+
+    inline float* dividir (const int &qtdDivisoes, const float intervalo[2]);
+    inline float mudarVariavelGL (const float &inicio, const float &fim, const int &valor);
+
+    inline float gaussLegendreN1 (const float &inicio, const float &fim, expre::expre *equacao);
+    inline float gaussLegendreN2 (const float &inicio, const float &fim, expre::expre *equacao);
+    inline float gaussLegendreN3 (const float &inicio, const float &fim, expre::expre *equacao);
+    inline float gaussLegendreN4 (const float &inicio, const float &fim, expre::expre *equacao);
+
+    //inline float exponencialSimplesNC (const int &grau, const int &filosofia, const int &qtdDivisoes, const float intervalo[2], expre::expre *equacao);
+    //inline float exponencialSimplesGL (const int &qtdPontos, const int &qtdDivisoes, const float intervalo[2], expre::expre *equacao);
 
 /**************************************************************************************************************************
---------------------------------------------------------- Funções ---------------------------------------------------------
+------------------------------------------------------ Newton Cotes -------------------------------------------------------
 **************************************************************************************************************************/
 
     
-    /*! Calcula A integral por Newton Cotes
-    ** Parâmetros: O grau do polinômio, a filosofia que pode ser ABERTA ou FECHADA, quantas divisões e o intervalo
+    /*! Calcula a integral por Newton Cotes
+    ** Parâmetros: O grau do polinômio, a filosofia que pode ser ABERTA ou FECHADA, quantas divisões, intervalo e a expressão
     ** Retornos: O valor da integral calculada
     */
     float newtonCotes(const int &grau, const int &filosofia, const int &qtdDivisoes, const float intervalo[2], expre::expre *equacao) {
@@ -206,10 +242,10 @@ namespace inte {
     }
 
     /*! Calcula Newton Cotes de grau 1 para o intervalo dado
-    ** Parâmetros: A filosofia que pode ser ABERTA ou FECHADA, o inicio e o fim do intervalo e a distância entre pontos
+    ** Parâmetros: A filosofia que pode ser ABERTA ou FECHADA, o inicio e o fim do intervalo, distância entre pontos e a expressão
     ** Retornos: O valor da integral para o intervalo dado
     */
-    inline float newtonCotesG1(const int &filosofia, const float &inicio, const float &fim, const float &h, expre::expre *equacao) {
+    inline float newtonCotesG1 (const int &filosofia, const float &inicio, const float &fim, const float &h, expre::expre *equacao) {
         float val1, val2;
 
         switch (filosofia){
@@ -227,10 +263,10 @@ namespace inte {
     }
 
     /*! Calcula Newton Cotes de grau 2 para o intervalo dado
-    ** Parâmetros: A filosofia que pode ser ABERTA ou FECHADA, o inicio e o fim do intervalo e a distância entre pontos
+    ** Parâmetros: A filosofia que pode ser ABERTA ou FECHADA, o inicio e o fim do intervalo, distância entre pontos e a expressão
     ** Retornos: O valor da integral para o intervalo dado
     */
-    float newtonCotesG2(const int &filosofia, const float &inicio, const float &fim, const float &h, expre::expre *equacao) {
+    float newtonCotesG2 (const int &filosofia, const float &inicio, const float &fim, const float &h, expre::expre *equacao) {
         float val1, val2, val3;
 
         switch (filosofia){
@@ -252,10 +288,10 @@ namespace inte {
     }
 
     /*! Calcula Newton Cotes de grau 3 para o intervalo dado
-    ** Parâmetros: A filosofia que pode ser ABERTA ou FECHADA, o inicio e o fim do intervalo e a distância entre pontos
+    ** Parâmetros: A filosofia que pode ser ABERTA ou FECHADA, o inicio e o fim do intervalo, distância entre pontos e a expressão
     ** Retornos: O valor da integral para o intervalo dado
     */
-    float newtonCotesG3(const int &filosofia, const float &inicio, const float &fim, const float &h, expre::expre *equacao) {
+    float newtonCotesG3 (const int &filosofia, const float &inicio, const float &fim, const float &h, expre::expre *equacao) {
         float val1, val2, val3, val4;
 
         switch (filosofia){
@@ -279,10 +315,10 @@ namespace inte {
     }
 
     /*! Calcula Newton Cotes de grau 4 para o intervalo dado
-    ** Parâmetros: A filosofia que pode ser ABERTA ou FECHADA, o inicio e o fim do intervalo e a distância entre pontos
+    ** Parâmetros: A filosofia que pode ser ABERTA ou FECHADA, o inicio e o fim do intervalo, distância entre pontos e a expressão
     ** Retornos: O valor da integral para o intervalo dado
     */
-    float newtonCotesG4(const int &filosofia, const float &inicio, const float &fim, const float &h, expre::expre *equacao) {
+    float newtonCotesG4 (const int &filosofia, const float &inicio, const float &fim, const float &h, expre::expre *equacao) {
         float val1, val2, val3, val4, val5;
 
         switch (filosofia){
@@ -307,6 +343,117 @@ namespace inte {
         }
     }
 
+/**************************************************************************************************************************
+------------------------------------------------------ Newton Cotes -------------------------------------------------------
+**************************************************************************************************************************/
+
+    /*! Calcula a integral por Gauss Legendre
+    ** Parâmetros: A quantidade de pontos, quantas divisões, o intervalo e a expressão
+    ** Retornos: O valor da integral calculada
+    */
+    float gaussLegendre (const int &qtdPontos, const int &qtdDivisoes, const float intervalo[2], expre::expre *equacao) {
+        float resul = 0, *divisoes = dividir(qtdDivisoes, intervalo);
+
+        switch (qtdPontos){
+            case 1:
+                for(int i = 0; i < qtdDivisoes; i++){
+                    resul += gaussLegendreN1(divisoes[i*2], divisoes[(i*2)+1], equacao);
+                }
+            break;
+            case 2:
+                for(int i = 0; i < qtdDivisoes; i++){
+                    resul += gaussLegendreN2(divisoes[i*2], divisoes[(i*2)+1], equacao);
+                }
+            break;
+            case 3:
+                for(int i = 0; i < qtdDivisoes; i++){
+                    resul += gaussLegendreN3(divisoes[i*2], divisoes[(i*2)+1], equacao);
+                }
+            break;
+            case 4:
+                for(int i = 0; i < qtdDivisoes; i++){
+                    resul += gaussLegendreN4(divisoes[i*2], divisoes[(i*2)+1], equacao);
+                }
+            break;
+        }
+
+        return resul;
+    }
+
+    /*! Faz as divisões do intervalo
+    ** Parâmetros: A quantidade de divisões e o intervalo
+    ** Retornos: Os intervalos divididos
+    */
+    float* dividir (const int &qtdDivisoes, const float intervalo[2]) {
+        float *divisoes = (float*)malloc(sizeof(float) * qtdDivisoes * 2),
+              tamInt = (intervalo[1] - intervalo[0]) / qtdDivisoes, aux = intervalo[0];
+
+        for(int i = 0; i < qtdDivisoes; i++){
+            divisoes[i*2] = aux;
+            aux += tamInt;
+            divisoes[(i*2)+1] = aux;
+        }
+        
+        return divisoes;
+    }
+
+    /*! Faz a mudança de variável para o cálculo de Gauss Legendre
+    ** Parâmetros: O início e o fim do intervalo e o valor da variável
+    ** Retornos: O resultado da mudança de variável
+    */
+    float mudarVariavelGL (const float &inicio, const float &fim, const int &valor) {
+        return ((fim + inicio) / 2) + (valor * (fim - inicio) / 2);
+    }
+
+    /*! Resolve a integral de Gauss Legendre com 1 ponto
+    ** Parâmetros: O início e o fim do intervalo e a expressão
+    ** Retornos: O resultado da integral
+    */
+    float gaussLegendreN1 (const float &inicio, const float &fim, expre::expre *equacao) {
+        return ((fim - inicio) / 2) * (w11 * equacao->calcular(mudarVariavelGL(inicio, fim, t11)));
+    }
+
+    /*! Resolve a integral de Gauss Legendre com 2 ponto
+    ** Parâmetros: O início e o fim do intervalo e a expressão
+    ** Retornos: O resultado da integral
+    */
+    float gaussLegendreN2 (const float &inicio, const float &fim, expre::expre *equacao) {
+        float resul = 0;
+
+        resul += (w12 * equacao->calcular(mudarVariavelGL(inicio, fim, t12)));
+        resul += (w22 * equacao->calcular(mudarVariavelGL(inicio, fim, t22)));
+
+        return ((fim - inicio) / 2) * resul;
+    }
+
+    /*! Resolve a integral de Gauss Legendre com 3 ponto
+    ** Parâmetros: O início e o fim do intervalo e a expressão
+    ** Retornos: O resultado da integral
+    */
+    float gaussLegendreN3 (const float &inicio, const float &fim, expre::expre *equacao) {
+        float resul = 0;
+
+        resul += (w13 * equacao->calcular(mudarVariavelGL(inicio, fim, t13)));
+        resul += (w23 * equacao->calcular(mudarVariavelGL(inicio, fim, t23)));
+        resul += (w33 * equacao->calcular(mudarVariavelGL(inicio, fim, t33)));
+
+        return ((fim - inicio) / 2) * resul;
+    }
+
+    /*! Resolve a integral de Gauss Legendre com 4 ponto
+    ** Parâmetros: O início e o fim do intervalo e a expressão
+    ** Retornos: O resultado da integral
+    */
+    float gaussLegendreN4 (const float &inicio, const float &fim, expre::expre *equacao) {
+        float resul = 0;
+
+        resul += (w14 * equacao->calcular(mudarVariavelGL(inicio, fim, t14)));
+        resul += (w24 * equacao->calcular(mudarVariavelGL(inicio, fim, t24)));
+        resul += (w34 * equacao->calcular(mudarVariavelGL(inicio, fim, t34)));
+        resul += (w44 * equacao->calcular(mudarVariavelGL(inicio, fim, t44)));
+
+        return ((fim - inicio) / 2) * resul;
+    }
 }
 
 #endif //INTEGRAL_H
--- algebra/testeIntegral.cpp
+++ algebra/testeIntegral.cpp
@@ -7,12 +7,12 @@
 int main(){
     float intervalo[2] = {0, (PI / 2)};
     
-    std::string tt = "1-3.2-senx";
+    std::string tt = "4+senx";
     if(parser::analisador(tt)){
         expre::expre *calc = parser::parser(tt, 0, tt.size());
 
-        std::cout << inte::newtonCotes(2, ABERTA, 1, intervalo, calc) << '\n';
-        std::cout << calc->calcular(2) << '\n';
+        std::cout << "Integral com Newton Cotes: " << inte::newtonCotes(2, ABERTA, 1, intervalo, calc) << '\n';
+        std::cout << "Integral com Gauss Legendre: " << inte::gaussLegendre(2, 1, intervalo, calc) << '\n';
     }else{
         std::cout << "Não foi\n";
     }

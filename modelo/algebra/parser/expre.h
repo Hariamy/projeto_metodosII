cdfe387945ea91c412e60e8e51da2d4d3b6ec0e6
icaroslb <icrsbarbosa@gmail.com>
2019-05-27 17:27:36 -0300

    Melhoramento do parser

--- modelo/algebra/parser/expre.h
+++ modelo/algebra/parser/expre.h
@@ -4,8 +4,16 @@
 #include <cmath>
 
 namespace expre {
+    #define BINARIA            0
+    #define UNARIA             1
+    #define PARENTESESABRINDO  2
+    #define PARENTESESFECHANDO 3
+    #define CONSTANTE          4
+    #define VARIAVEL           5
+
     class expre {
     public:
+        int tipo;
         inline virtual float calcular (const float &x) = 0;
     };
 
@@ -50,6 +58,7 @@ namespace expre {
 
     class variavel: public expre {
     public:
+        variavel ();
         inline float calcular (const float &x);
     };
 
@@ -61,13 +70,13 @@ namespace expre {
     ** Parâmetros: As duas operações filhas
     ** Retornos: void
     */
-    binario::binario (expre *dir, expre *esq) : filhoDir(dir), filhoEsq(esq) {}
+    binario::binario (expre *dir, expre *esq) : filhoDir(dir), filhoEsq(esq) {tipo = BINARIA;}
 
     /*! Inicializa as operações binárias com valores nulos
     ** Parâmetros: void
     ** Retornos: void
     */
-    binario::binario () : filhoDir(NULL), filhoEsq(NULL) {}
+    binario::binario () : filhoDir(NULL), filhoEsq(NULL) {tipo = BINARIA;}
 
     /*! Insere filho direito
     ** Parâmetros: A operação filha direita
@@ -89,13 +98,13 @@ namespace expre {
     ** Parâmetros: A operação filha
     ** Retornos: void
     */
-    unario::unario (expre *novo) : filho(novo) {}
+    unario::unario (expre *novo) : filho(novo) {tipo = UNARIA;}
 
     /*! Inicializa as operações unárias com valores nulos
     ** Parâmetros: void
     ** Retornos: void
     */
-    unario::unario () : filho(NULL) {}
+    unario::unario () : filho(NULL) {tipo = UNARIA;}
 
     /*! Insere filho
     ** Parâmetros: A operação filha
@@ -109,7 +118,7 @@ namespace expre {
     ** Parâmetros: O valor da constante
     ** Retornos: void
     */
-    constante::constante (float novo) : valor(novo) {}
+    constante::constante (float novo) : valor(novo) {tipo = CONSTANTE;}
 
     /*! Insere um novo valor como constante
     ** Parâmetros: O valor da constante
@@ -126,6 +135,8 @@ namespace expre {
     float constante::calcular (const float &x) {
         return valor;
     }
+
+    variavel::variavel () { tipo = VARIAVEL;}
 
     /*! Retorna o valor da variável
     ** Parâmetros: O valor da variável
--- modelo/algebra/parser/parser.h
+++ modelo/algebra/parser/parser.h
@@ -2,27 +2,151 @@
 #define PARSER_H
 
 #include <string>
+#include <iostream>
+#include <vector>
 #include "expre.h"
-#include "binario.h"
 #include "unario.h"
+#include "binario.h"
 
 namespace parser {
-
-    struct pilhaExpre {
+    struct noPilha {
         expre::expre *val;
-        struct pilhaExpre *ant, *prox;
+        struct noPilha *ant, *prox;
     };
 
-    #define BINARIA            0
-    #define UNARIA             1
-    #define PARENTESESABRINDO  2
-    #define PARENTESESFECHANDO 3
-    #define CONSTANTE          4
-    #define VARIAVEL           5
+    struct pilha {
+        noPilha *base, *topo;
+        int tam;
+
+        inline void init ();
+
+        inline void inserir (expre::expre *no);
+        inline void inserir (noPilha *no);
+        inline void reInserir (noPilha *no);
+        inline noPilha* retirarTopo ();
+        inline noPilha* retirarBase ();
+
+        inline bool estaVazio ();
+        inline bool naoEstaVazio ();
+        inline int tamanho ();
+
+        inline ~pilha();
+    };
 
     inline bool analisador (std::string &expressao);
 
-    inline expre::expre* parser (std::string &expressao);
+    inline float lerConstante (std::string &expressao, int &percorrer);
+    inline pilha* contruirPilha (std::string &expressao);
+    inline expre::expre* construirArvore (pilha *expressaoPoIn);
+
+
+    void pilha::init () {
+        tam = 0;
+        base = NULL;
+        topo = NULL;
+    }
+
+    void pilha::inserir (expre::expre *no) {
+        noPilha *novo = (noPilha*)malloc(sizeof(noPilha));
+        
+        novo->val = no;
+        inserir(novo);
+    }
+
+    void pilha::inserir (noPilha *no) {
+        no->ant = topo;
+        no->prox = NULL;
+        
+        if(base == NULL){
+            base = no;
+        }else{
+            topo->prox = no;
+        }
+
+        topo = no;
+
+        tam++;
+    }
+
+    void pilha::reInserir (noPilha *no) {
+        if(no->ant != NULL){
+            no->ant->prox = no->prox;
+        }else{
+            base = no->prox;
+        }
+
+
+        if(no->prox != NULL){
+            no->prox->ant = no->ant;
+        }else{
+            topo = no->ant;
+        }
+
+        tam--;
+
+        inserir(no);
+    }
+
+    noPilha* pilha::retirarTopo () {
+        noPilha *retorno = topo;
+
+        if(topo != NULL){
+            topo = topo->ant;
+
+            if(topo != NULL){
+                topo->prox = NULL;
+            }else{
+                base = NULL;
+            }
+
+            tam--;
+        }
+
+        return retorno;
+    }
+
+    noPilha* pilha::retirarBase () {
+        noPilha *retorno = base;
+
+        if(base != NULL){
+            base = base->prox;
+
+            if(retorno->prox != NULL){
+                retorno->prox->ant = NULL;
+            }
+        }
+
+        tam--;
+
+        return retorno;
+    }
+
+    bool pilha::estaVazio () {
+        return (tam == 0);
+    }
+    
+    bool pilha::naoEstaVazio () {
+        return (tam > 0);
+    }
+
+    int pilha::tamanho () {
+        return tam;
+    }
+
+    pilha::~pilha() {
+        noPilha *liberar = NULL, *percorrer = base;
+
+        while(percorrer != NULL) {
+            liberar = percorrer;
+            percorrer = percorrer->prox;
+
+            free(liberar);
+        }
+    }
+
+
+
+
 
     /*! Analiza se a expressão está corretamente estruturada
     ** Parâmetros: A expressão em string
@@ -117,112 +241,133 @@ namespace parser {
             return true;
     }
 
-    /*! Constroi a árvore de expressão da respectiva string
-    ** Parâmetros: A expressão em string
-    ** Retornos: A árvore de expressão construída
-    */
-    expre::expre* parser (std::string &expressao, const int &inicio, const int &fim) {
-        expre::expre *resul;
-
-        if(inicio == fim){
-            return new expre::constante(0);
-        }
-
-        for(int i = inicio; i < fim; i++){
-            if(expressao[i] < '0' || expressao[i] > '9'){
-                switch (expressao[i]){
-                    case '+':
-                        return new expre::soma(parser(expressao, inicio, i), parser(expressao, i+1, fim));
-                    break;
-                    case '-':
-                        return new expre::subtracao(parser(expressao, inicio, i), parser(expressao, i+1, fim));
-                    break;
-                    case '*':
-                        return new expre::multiplicacao(parser(expressao, inicio, i), parser(expressao, i+1, fim));
-                    break;
-                    case '/':
-                        return new expre::divisao(parser(expressao, inicio, i), parser(expressao, i+1, fim));
-                    break;
-                    case '^':
-                        return new expre::potencia(parser(expressao, inicio, i), parser(expressao, i+1, fim));
-                    break;
-                    case 'x':
-                        return new expre::variavel();
-                    break;
-                    default:
-                        if(!expressao.compare(i, 3, "sen")){
-                            return new expre::sen(parser(expressao, i+3, fim));
-                        }else if(!expressao.compare(i, 3, "cos")){
-                            return new expre::cos(parser(expressao, i+3, fim));
-                        }else if(!expressao.compare(i, 3, "tan")){
-                            return new expre::tan(parser(expressao, i+3, fim));
-                        }else if(!expressao.compare(i, 3, "log")){
-                            return new expre::log(parser(expressao, i+3, fim));
-                        }
-                    break;
-                }
-            }
-        }
+    float lerConstante (std::string &expressao, int &percorrer) {
+        int quant = 0, inicio = percorrer;
         
-        return new expre::constante(atof((expressao.substr(inicio, fim - inicio)).data()));
+        while((expressao[percorrer] >= '0' && expressao[percorrer] <= '9') || expressao[percorrer] == '.'){
+            quant++;
+            percorrer++;
+        }
+
+        return atof(expressao.substr(inicio, quant).data());
     }
 
-    expre::expre* parserAux (std::string &expressao, const int &inicio, const int &fim) {
-        pilhaExpre *basePilhaTemporaria = NULL, *topoPilhaTemporaria = NULL,
-                   *basePilha = NULL, *topoPilha = NULL;
-        int percorrer = inicio, quant = 0;
+    pilha* contruirPilha (std::string &expressao) {
+        pilha *pilhaTemporaria = (pilha*)malloc(sizeof(pilha)),
+              *pilhaFinal = (pilha*)malloc(sizeof(pilha));
+        noPilha *auxTrans;
+        int percorrer = 0, tam = expressao.size(), quant = 0;
+        expre::expre *ope;
+
+        pilhaTemporaria->init();
+        pilhaFinal->init();
         
-        while(percorrer < fim){
+        while(percorrer < tam){
             if(expressao[percorrer] >= '0' && expressao[percorrer] <= '9'){
-                //inserirPilha(criarConstante(expressao, percorrer));
+                pilhaFinal->inserir( ope = ( new expre::constante( lerConstante (expressao, percorrer) ) ) );
                 quant++;
             }else{
                 switch (expressao[percorrer]){
                 case '+':
-                    //inserirPilhaTemporaria(criarSoma());
+                    pilhaTemporaria->inserir( ope = ( new expre::soma() ) );
+                    percorrer++;
                 break;
                 case '-':
-                    //inserirPilhaTemporaria(criarSubtracao());
+                    pilhaTemporaria->inserir( ope = ( new expre::subtracao() ) );
+                    percorrer++;
                 break;
                 case '*':
-                    //inserirPilhaTemporaria(criarMultiplicacao());
+                    pilhaTemporaria->inserir( ope = ( new expre::multiplicacao() ) );
+                    percorrer++;
                 break;
                 case '/':
-                    //inserirPilhaTemporaria(criarDivisao());
+                    pilhaTemporaria->inserir( ope = ( new expre::divisao() ) );
+                    percorrer++;
                 break;
                 case '^':
-                    //inserirPilhaTemporaria(criarPotencia());
+                    pilhaTemporaria->inserir( ope = ( new expre::potencia() ) );
+                    percorrer++;
                 break;
                 case 'x':
-                    //inserirPilha(criarVariavel();
+                    pilhaFinal->inserir( ope = ( new expre::variavel() ) );
+                    percorrer++;
                     quant++;
                 break;
                 default:
                     if(!expressao.compare(percorrer, 3, "sen")){
-                            //inserirPilhaTemporaria(criarSen());
+                            pilhaTemporaria->inserir( ope = ( new expre::sen() ) );
+                            percorrer += 3;
                         }else if(!expressao.compare(percorrer, 3, "cos")){
-                            //inserirPilhaTemporaria(criarCos());
+                            pilhaTemporaria->inserir( ope = ( new expre::cos() ) );
+                            percorrer += 3;
                         }else if(!expressao.compare(percorrer, 3, "tan")){
-                            //inserirPilhaTemporaria(criarTan());
+                            pilhaTemporaria->inserir( ope = ( new expre::tan() ) );
+                            percorrer += 3;
                         }else if(!expressao.compare(percorrer, 3, "log")){
-                            //inserirPilhaTemporaria(criarLog());
+                            pilhaTemporaria->inserir( ope = ( new expre::log() ) );
+                            percorrer += 3;
                         }
                 break;
                 }
             }
-
-            if(topoPilhaTemporaria == NULL && basePilhaTemporaria == NULL){
-                basePilhaTemporaria = topoPilhaTemporaria;
-            }
-            if(topoPilha == NULL && basePilha == NULL){
-                basePilha = topoPilha;
-            }
-
+            
             if(quant >= 2){
-                //inserirPilha();
+                pilhaFinal->inserir(pilhaTemporaria->retirarTopo());
+                quant = 1;
             }
         }
+
+        while(pilhaTemporaria->naoEstaVazio()){
+            pilhaFinal->inserir(pilhaTemporaria->retirarTopo());
+        }
+
+        return pilhaFinal;
     }
+
+    expre::expre* construirArvore (pilha *expressaoPoIn) {
+        noPilha *lido = NULL, aux;
+        std::vector<expre::expre*> nosLidos;
+
+        while(expressaoPoIn->naoEstaVazio()){
+            lido = expressaoPoIn->retirarBase();
+
+            switch(lido->val->tipo){
+                case BINARIA:
+                    ((expre::binario*)lido->val)->inserirDir(nosLidos.back());
+                    nosLidos.pop_back();
+
+                    ((expre::binario*)lido->val)->inserirEsq(nosLidos.back());
+                    nosLidos.pop_back();
+
+                    nosLidos.push_back(lido->val);
+                break;
+                case UNARIA:
+                    ((expre::unario*)lido->val)->inserirFilho(nosLidos.back());
+                    nosLidos.pop_back();
+
+                    nosLidos.push_back(lido->val);
+                break;
+                default:
+                    nosLidos.push_back(lido->val);
+                break;
+
+                free(lido);
+            }
+        }
+
+        return nosLidos.back();
+    }
+}
+
+int main () {
+    std::string t = "1+2*3+x";
+    parser::pilha *p = parser::contruirPilha(t);
+    parser::noPilha *aux = p->base;
+    expre::expre *arvo = construirArvore(p);
+
+    std::cout << arvo->calcular(1) << '\n';
+
+    return 0;
 }
 
 #endif //PARSER_H

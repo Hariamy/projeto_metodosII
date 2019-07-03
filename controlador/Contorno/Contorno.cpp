63a31ced6e0ade77d7b81520cef145da84a8b5e5
italo <italo@LAPTOP-78V9G3GA.localdomain>
2019-07-03 12:12:28 -0300

    edp 2D

--- controlador/Contorno/Contorno.cpp
+++ controlador/Contorno/Contorno.cpp
@@ -70,7 +70,78 @@ void resolverSistema(MatrixXf &matriz, VectorXf &vetor) {
     }
 }
 
+VectorXf Contorno2D(double xi, double xf, double yi, double yf, double uContorno, double coefLapl,
+                    funcaoReal2D coefDxDy, funcaoReal2D coefDx, funcaoReal2D coefDy, 
+                    funcaoReal2D coefU, funcaoReal2D F,  int nparticoesx, int nparticoesy){
+    int dim = (nparticoesy-1)*(nparticoesx-1), i, j;
+    double x, y, dx = (xf - xi)/nparticoesx, dy = (yf - yi)/nparticoesy;
+    VectorXf u(dim),
+             b(dim);
+    MatrixXf A(dim, dim);
+
+    for(i=0; i<dim; i++){
+        x = xi + (i%(nparticoesx-1))*dx;
+        y = yi + (i/(nparticoesx-1))*dy;
+        b(i) = -F(x, y);
+        if(i%(nparticoesx-1) == 0){
+            // b(i) -= (coefDxDy(x,y)/(4*dx*dy))*uContorno;
+            b(i) -= ( coefLapl(x,y)/(dx*dx) - coefDx(x,y)/(2*dx) )*uContorno;
+            // b(i) -= -(coefDxDy(x,y)/(4*dx*dy))*uContorno;
+            if (i/(nparticoesx-1) == 0)
+                b(i) -= (coefDxDy(x,y)/(4*dx*dy))*uContorno;
+        }
+        if(i%(nparticoesx-1) == nparticoesx-2){
+            // b(i) -= -(coefDxDy(x,y)/(4*dx*dy))*uContorno;
+            b(i) -= ( coefLapl(x,y)/(dx*dx) + coefDx(x,y)/(2*dx) )*uContorno;
+            // b(i) -= (coefDxDy(x,y)/(4*dx*dy))*uContorno;
+            if (i/(nparticoesx-1) == 0)
+                b(i) -= -(coefDxDy(x,y)/(4*dx*dy))*uContorno;
+        }
+        if(i/(nparticoesx-1) == 0){
+            // b(i) -= (coefDxDy(x,y)/(4*dx*dy))*uContorno;
+            b(i) -= ( coefLapl(x,y)/(dy*dy) - coefDy(x,y)/(2*dy) )*uContorno;
+            // b(i) -= -(coefDxDy(x,y)/(4*dx*dy))*uContorno;
+            if(i%(nparticoesx-1) == 0)
+                b(i) -= -(coefDxDy(x,y)/(4*dx*dy))*uContorno;
+        }
+        if(i/(nparticoesx-1) == nparticoesy-2){
+            // b(i) -= -(coefDxDy(x,y)/(4*dx*dy))*uContorno;
+            b(i) -= ( coefLapl(x,y)/(dy*dy) + coefDy(x,y)/(2*dy) )*uContorno;
+            // b(i) -= +(coefDxDy(x,y)/(4*dx*dy))*uContorno;
+            if(i%(nparticoesx-1) == nparticoesx-2)
+                b(i) -= (coefDxDy(x,y)/(4*dx*dy))*uContorno;
+        }
+
+        A(i,i) = -2( 1/(dx*dx) + 1/(dx*dx) );
+        if(i-1 >= 0) A(i,i-1) = coefLapl(x,y)/(dx*dx) - coefDx(x,y)/(2*dx);
+        if(i+1 < dim) A(i,i+1) = coefLapl(x,y)/(dx*dx) + coefDx(x,y)/(2*dx);
+        
+        if(i - nparticoesx < dim) 
+            A(i,i - nparticoesx) = coefDxDy(x,y)/(4*dx*dy);
+        if(i - nparticoesx+1 < dim) 
+            A(i,i - nparticoesx+1) = coefLapl(x,y)/(dy*dy) - coefDy(x,y)/(2*dy);
+        if(i - nparticoesx+2 < dim) 
+            A(i,i - nparticoesx+2) = -coefDxDy(x,y)/(4*dx*dy);
+
+        if(i + nparticoesx-2 < dim)
+            A(i,i - nparticoesx+2) = -coefDxDy(x,y)/(4*dx*dy);
+        if(i + nparticoesx-1 < dim) 
+            A(i,i + nparticoesx-1) = coefLapl(x,y)/(dy*dy) + coefDy(x,y)/(2*dy);
+        if(i + nparticoesx < dim)
+            A(i,i - nparticoesx+2) = coefDxDy(x,y)/(4*dx*dy);
+
+    }
+
+    u = A.inverse() * b;
+
+    return u;
+}
+
+
 int main(){
+
+    
+
     VectorXf respo = Contorno1D(0.2, 0.5, 6);
     
     std::cout << respo << std::endl;
--- controlador/Contorno/Contorno.h
+++ controlador/Contorno/Contorno.h
@@ -9,10 +9,17 @@
 using namespace Eigen;
 
 typedef VectorXf (funcao)(VectorXf estado, double t);
+typedef double (funcaoReal2D)(double x, double y);
 
 MatrixXf preencherMatriz(double xi, int particoes, double deltaR);
 
 void resolverSistema(MatrixXf &matriz, VectorXf &vetor);
+
+VectorXf Contorno1D(double xi, double xf,  int particoes);
+
+VectorXf Contorno2D(double xi, double xf, double yi, double yf, double uContorno, double coefLapl,
+					funcaoReal2D coefDxDy, funcaoReal2D coefDx, funcaoReal2D coefDy, 
+					funcaoReal2D coefU, funcaoReal2D F,  int nparticoesx, int nparticoesy);
 
 #endif //PVC
 
